643d5371de81211d8a8089226864522d8d20af6a
nihce <miha.klokocovnik@gmail.com>
2019-11-28 11:56:25 +0100

    copy of original code and solved first task

---
+++ src/bitcoin-prednaloga/bin/sh002.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+./bitcoin-cli -regtest generate 150
+#store text from file to variable data and convert it to clean HEX form
+data=$(xxd -p -u data.txt)
+#create new transaction and add data to it, then store it
+transaction=$(./bitcoin-cli -regtest createrawtransaction "[]" "{\"data\":\"$data\"}")
+#echo $transaction
+#add mining fee to transaction and store funded transaction
+funded=$(./bitcoin-cli -regtest fundrawtransaction $transaction | jq -r '.hex')
+#echo $funded
+#sign transaction
+signed=$(./bitcoin-cli -regtest signrawtransaction $funded)
+# echo $signed
+#show final transaction in human readable form
+rawTransaction=$(echo -n $signed | jq -r '.hex')
+# echo $rawTransaction
+./bitcoin-cli -regtest decoderawtransaction $rawTransaction
